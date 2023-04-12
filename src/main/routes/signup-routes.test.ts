4bcff76a5327bf5fe9ac98b3c84736defaeb9145
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-12 01:17:37 -0300

    test: ensure LogControllerDecorator calls controller with correct value

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 
-describe("SignUP Routes", () => {
+describe("SignUp Routes", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
