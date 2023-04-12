b099a611bce2e47a3a76afc7f94cda9e18ba477c
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-11 21:37:32 -0300

    feat: ensure cors as enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import { bodyParser, bodyParserEncoded } from "../middlewares/body-parser";
+import { cors } from "../middlewares/cors";
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(bodyParserEncoded);
+  app.use(cors);
 };
