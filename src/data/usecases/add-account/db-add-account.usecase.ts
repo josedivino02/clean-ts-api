7c1b1ce0025453fcd16bb28f0604156c36e8e3a4
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-15 03:22:49 -0300

    refactor: change call constructor method

--- src/data/usecases/add-account/db-add-account.usecase.ts
+++ src/data/usecases/add-account/db-add-account.usecase.ts
@@ -7,16 +7,10 @@ import {
 } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter: Encrypter;
-  private readonly addAccountRepository: AddAccountRepository;
-
   constructor(
-    encrypter: Encrypter,
-    addAccountRepository: AddAccountRepository
-  ) {
-    this.encrypter = encrypter;
-    this.addAccountRepository = addAccountRepository;
-  }
+    private encrypter: Encrypter,
+    private addAccountRepository: AddAccountRepository
+  ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password);
--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -2,11 +2,7 @@ import { Encrypter } from "../../data/protocols/encrypter.repository";
 import bcrypt from "bcrypt";
 
 export class BcryptAdapter implements Encrypter {
-  private readonly salt: number;
-
-  constructor(salt: number) {
-    this.salt = salt;
-  }
+  constructor(private salt: number) {}
 
   async encrypt(value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt);
