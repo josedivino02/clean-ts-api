8fbe992372e8f33b120f33084ac11f1e0d80954e
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-16 20:15:26 -0300

    ensure DbAthentication returns null if HashComparer return false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -116,4 +116,16 @@ describe("DbAuthentication UseCase", () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  test("Should return null if HashComparer returns false", async () => {
+    const { sut, hashComparerStub } = makeSut();
+
+    jest
+      .spyOn(hashComparerStub, "compare")
+      .mockReturnValueOnce(Promise.resolve(false));
+
+    const accessToken = await sut.auth(makeFakeAuthentication());
+
+    expect(accessToken).toBeNull();
+  });
 });
