98a276cc2fdfadb451608bde9ebe85ba5c7e5159
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-21 02:21:58 -0300

    feat: ensure DbAddAccount calls LoadAccountByEMailRepository with correct email and return null if LoadAccountByEmailRepository not return null

--- src/data/usecases/add-account/db-add-account.usecase.ts
+++ src/data/usecases/add-account/db-add-account.usecase.ts
@@ -4,21 +4,31 @@ import {
   AccountModel,
   Hasher,
   AddAccountRepository,
+  LoadAccountByEmailRepository,
 } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.hasher.hash(accountData.password);
-
-    const account = await this.addAccountRepository.add(
-      Object.assign({}, accountData, { password: hashedPassword })
+    const account = await this.loadAccountByEmailRepository.loadByEmail(
+      accountData.email
     );
 
-    return account;
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password);
+
+      const newAccount = await this.addAccountRepository.add(
+        Object.assign({}, accountData, { password: hashedPassword })
+      );
+
+      return newAccount;
+    }
+
+    return null;
   }
 }
