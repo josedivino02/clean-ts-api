8c6da9ef383e38a7ecd028699ded9dc315fa57c1
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-21 17:00:44 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
+import { verify } from "crypto";
 
 jest.mock("jsonwebtoken", () => ({
   async sign(): Promise<string> {
     return Promise.resolve("any_token");
+  },
+
+  async verify(): Promise<string> {
+    return Promise.resolve("any_value");
   },
 }));
 
@@ -41,6 +46,18 @@ describe("Jwt Adapter", () => {
       const promise = sut.encrypt("any_id");
 
       await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe("verify()", () => {
+    test("Should call verify with correct values", async () => {
+      const sut = makeSut();
+
+      const verifySpy = jest.spyOn(jwt, "verify");
+
+      await sut.decrypt("any_token");
+
+      expect(verifySpy).toHaveBeenCalledWith("any_token", "secret");
     });
   });
 });
