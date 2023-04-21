17fff5612dfccffaa79a3248518eb57c3cf61587
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-21 13:49:13 -0300

    feat: add Survey composition

---
+++ src/main/factories/controller/add-survey/add-survey-validation.factory.spec.ts
@@ -0,0 +1,23 @@
+import { makeAddSurveyValidation } from "./add-survey-validation.factory";
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+} from "../../../../validation/validators";
+import { Validation } from "../../../../presentation/protocols/validation.interface";
+
+// mockar um modulo
+jest.mock("../../../../validation/validators/validation-composite");
+
+describe("AddSurveyValidation Factory", () => {
+  test("Should call ValidationComposite with all validations", () => {
+    makeAddSurveyValidation();
+
+    const validations: Validation[] = [];
+
+    for (const field of ["question", "answers"]) {
+      validations.push(new RequiredFieldValidation(field));
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
