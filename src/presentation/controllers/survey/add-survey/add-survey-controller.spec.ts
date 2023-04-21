1974eb153167ee1b532750c6a4f647f4b13dd678
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-21 12:18:43 -0300

    test: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,8 @@
-import { badRequest, serverError } from "./../../../helpers/http/http-helper";
+import {
+  badRequest,
+  noContent,
+  serverError,
+} from "./../../../helpers/http/http-helper";
 import {
   AddSurvey,
   AddSurveyModel,
@@ -102,5 +106,13 @@ describe("AddSurvey Controller", () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("Should return 204 on success", async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(noContent());
   });
 });
