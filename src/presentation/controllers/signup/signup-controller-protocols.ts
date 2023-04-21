4a3d912019014dc8a949292eb1ac17c793d9b9b2
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-21 01:38:06 -0300

    feat: ensre SignUpController calls Authentication with correct values

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -2,3 +2,4 @@ export * from "../../protocols";
 export * from "../../../domain/usecases/add-account.interface";
 export * from "../../../domain/models/account.interface";
 export * from "../../protocols/validation.interface";
+export * from "../../../domain/usecases/authentication.interface";
--- src/presentation/controllers/signup/signup.controller.ts
+++ src/presentation/controllers/signup/signup.controller.ts
@@ -1,3 +1,4 @@
+import { Authentication } from "./../../../domain/usecases/authentication.interface";
 import {
   HttpRequest,
   HttpResponse,
@@ -5,12 +6,19 @@ import {
   AddAccount,
   Validation,
 } from "./signup-controller-protocols";
-import { badRequest, serverError, ok } from "../../helpers/http/http-helper";
+import {
+  badRequest,
+  serverError,
+  ok,
+  forbidden,
+} from "../../helpers/http/http-helper";
+import { EmailInUseError } from "../../errors";
 
 export class SignUpController implements Controller {
   constructor(
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -27,7 +35,11 @@ export class SignUpController implements Controller {
         password,
       });
 
-      return ok(account);
+      if (!account) return forbidden(new EmailInUseError());
+
+      const accessToken = await this.authentication.auth({ email, password });
+
+      return ok({ accessToken });
     } catch (error) {
       return serverError(error);
     }
