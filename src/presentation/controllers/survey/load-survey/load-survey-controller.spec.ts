69f80555f959424199adfb112f90798038e9f16d
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-23 18:32:25 -0300

    test: ensure LoadSurveysContorller calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
@@ -0,0 +1,58 @@
+import { LoadSurveysController } from "./load-surveys-controller";
+import { LoadSurveys, SurveyModel } from "./load-survey-controller-protocols";
+import MockDate from "mockdate";
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: "any_id",
+      question: "any_question",
+      answers: [
+        {
+          image: "any_image",
+          answer: "any_answer",
+        },
+      ],
+      date: new Date(),
+    },
+    {
+      id: "other_id",
+      question: "other_question",
+      answers: [
+        {
+          image: "other_image",
+          answer: "other_answer",
+        },
+      ],
+      date: new Date(),
+    },
+  ];
+};
+
+describe("LoadSurveys Controller", () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test("Should call LoadSurveys", async () => {
+    class LoadSurveysSub implements LoadSurveys {
+      async load(): Promise<SurveyModel[]> {
+        return Promise.resolve(makeFakeSurveys());
+      }
+    }
+
+    const loadSurveyStub = new LoadSurveysSub();
+
+    const loadSpy = jest.spyOn(loadSurveyStub, "load");
+
+    const sut = new LoadSurveysController(loadSurveyStub);
+
+    await sut.handle({});
+
+    expect(loadSpy).toHaveBeenCalledWith();
+  });
+});
