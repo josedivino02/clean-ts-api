d2b406765213061a5962007c9e66772c38c2a163
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-26 17:58:46 -0300

    test: ensure DbSaveSurveyResult return a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -77,4 +77,12 @@ describe('DbSaveSurveyResult Usecase', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut();
+
+    const surveyResult = await sut.save(makeFakeSurveyResultData());
+
+    expect(surveyResult).toEqual(makeFakeSurveyResult());
+  });
 });
