9fa8ca04b65273c59927925a482fa5f991be2675
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-26 22:43:43 -0300

    feat: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo.repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo.repository.ts
@@ -1,25 +1,37 @@
-import { LoadSurveysRepository } from "@/data/protocols/db/survey/load-surveys.repository";
+import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys.repository';
 import {
   AddSurveyModel,
   AddSurveyRepository,
-} from "@/data/usecases/add-survey/db-add-survey-protocols";
-import { SurveyModel } from "@/domain/models/survey.interface";
-import { MongoHelper } from "../helpers/mongo.helper";
+} from '@/data/usecases/add-survey/db-add-survey-protocols';
+import { SurveyModel } from '@/domain/models/survey.interface';
+import { MongoHelper } from '../helpers/mongo.helper';
+import { LoadSurveyByIdRepository } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols';
 
 export class SurveyMongoRepository
-  implements AddSurveyRepository, LoadSurveysRepository
+  implements
+    AddSurveyRepository,
+    LoadSurveysRepository,
+    LoadSurveyByIdRepository
 {
   async add(surveyData: AddSurveyModel): Promise<void> {
-    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveyCollection = await MongoHelper.getCollection('surveys');
 
     await surveyCollection.insertOne(surveyData);
   }
 
   async loadAll(): Promise<SurveyModel[]> {
-    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveyCollection = await MongoHelper.getCollection('surveys');
 
-    const surveys: SurveyModel[] = await surveyCollection.find().toArray();
+    const surveys = await surveyCollection.find().toArray();
 
     return surveys;
+  }
+
+  async loadById(id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+
+    const survey = await surveyCollection.findOne({ _id: id });
+
+    return survey;
   }
 }
