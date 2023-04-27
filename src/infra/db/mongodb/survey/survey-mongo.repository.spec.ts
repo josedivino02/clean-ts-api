eabfa23d97965f23b7ec3124d5aa088ebee93ffc
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-26 22:44:07 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo.repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo.repository.spec.ts
@@ -1,6 +1,6 @@
-import { Collection } from "mongodb";
-import { MongoHelper } from "../helpers/mongo.helper";
-import { SurveyMongoRepository } from "./survey-mongo.repository";
+import { Collection } from 'mongodb';
+import { MongoHelper } from '../helpers/mongo.helper';
+import { SurveyMongoRepository } from './survey-mongo.repository';
 
 let surveyCollection: Collection;
 
@@ -8,7 +8,7 @@ const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository();
 };
 
-describe("Survey Mongo Repository", () => {
+describe('Survey Mongo Repository', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL));
 
   afterAll(async () => {
@@ -16,62 +16,62 @@ describe("Survey Mongo Repository", () => {
   });
 
   beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection("surveys");
+    surveyCollection = await MongoHelper.getCollection('surveys');
 
     await surveyCollection.deleteMany({});
   });
 
-  describe("add()", () => {
-    test("Should add a survey on success", async () => {
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
       const sut = makeSut();
 
       await sut.add({
-        question: "any_question",
+        question: 'any_question',
         answers: [
           {
-            image: "any_image",
-            answer: "any_answer",
+            image: 'any_image',
+            answer: 'any_answer',
           },
           {
-            answer: "other_answer",
+            answer: 'other_answer',
           },
         ],
         date: new Date(),
       });
 
       const survey = await surveyCollection.findOne({
-        question: "any_question",
+        question: 'any_question',
       });
 
       expect(survey).toBeTruthy();
     });
   });
 
-  describe("loadAll()", () => {
-    test("Should load all surveys on success", async () => {
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
       await surveyCollection.insertMany([
         {
-          question: "any_question",
+          question: 'any_question',
           answers: [
             {
-              image: "any_image",
-              answer: "any_answer",
+              image: 'any_image',
+              answer: 'any_answer',
             },
             {
-              answer: "other_answer",
+              answer: 'other_answer',
             },
           ],
           date: new Date(),
         },
         {
-          question: "other_question",
+          question: 'other_question',
           answers: [
             {
-              image: "other_image",
-              answer: "other_answer",
+              image: 'other_image',
+              answer: 'other_answer',
             },
             {
-              answer: "other_answer",
+              answer: 'other_answer',
             },
           ],
           date: new Date(),
@@ -87,12 +87,36 @@ describe("Survey Mongo Repository", () => {
       // expect(surveys[1].length).toBe("other_question");
     });
 
-    test("Should load empty list", async () => {
+    test('Should load empty list', async () => {
       const sut = makeSut();
 
       const surveys = await sut.loadAll();
 
       expect(surveys.length).toBe(0);
+    });
+  });
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        date: new Date(),
+      });
+
+      const sut = makeSut();
+
+      const survey = await sut.loadById(res.ops[0]._id);
+
+      expect(survey).toBeTruthy();
     });
   });
 });
