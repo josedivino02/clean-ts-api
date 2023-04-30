86d9eed7624134aaa38c9a56a36b305683298bf5
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-04-30 17:21:08 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { accountSchema } from './schemas/account-schema';
+import { loginPath } from './paths/login-path';
+import { loginParamsSchema } from './schemas/login-params-schema';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'Curso envolvendo boas práticas com Node',
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
