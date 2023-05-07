3bed73ff136b54d0c96e72b1dfdb0cb4b0d6aaab
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-05-07 19:30:49 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,6 +1,7 @@
 export const surveyResultPath = {
   put: {
-    tags: ['Login'],
+    security: [{ apiKeyAuth: [] }],
+    tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
     parameters: [
       {
@@ -21,6 +22,43 @@ export const surveyResultPath = {
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
+
+  get: {
+    security: [{ apiKeyAuth: [] }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
