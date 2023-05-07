10023a2785f792cf196540a73a0457d65389e481
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-05-07 18:56:12 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResoult with correct value

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result.controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.controller.ts
@@ -7,19 +7,27 @@ import {
   HttpRequest,
   HttpResponse,
   LoadSurveyById,
+  LoadSurveyResult,
 } from './load-survey-result-protocols';
 import { InvalidParamError } from '@/presentation/errors';
 
 export class LoadSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyById: LoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const { surveyId } = httpRequest.params;
+
       const survey = await this.loadSurveyById.loadById(
         httpRequest.params.surveyId
       );
 
       if (!survey) return forbidden(new InvalidParamError('surveyId'));
+
+      await this.loadSurveyResult.load(surveyId);
 
       return null;
     } catch (error) {
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -4,6 +4,7 @@ import {
   SaveSurveyResultParams,
 } from '@/domain/usecases/survey-result/save-survey-result.interface';
 import { SurveyResultModel } from '@/domain/models/survey-result.interface';
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result.interface';
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
   class SaveSurveyResultStub implements SaveSurveyResult {
@@ -13,4 +14,14 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
   }
 
   return new SaveSurveyResultStub();
+};
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load(surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel());
+    }
+  }
+
+  return new LoadSurveyResultStub();
 };
