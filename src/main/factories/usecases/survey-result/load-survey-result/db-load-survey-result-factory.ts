d5bb8f7e23f18996298b9aadf1412dcda7d9ddb9
josedivino02 <68295910+josedivino02@users.noreply.github.com>
2023-05-07 19:23:12 -0300

    feat: ensure load survey route retuns 403 if no accessToken is provided

---
+++ src/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory.ts
@@ -0,0 +1,14 @@
+import { DbLoadSurveyResult } from '@/data/usecases/survey-result/load-survey-result/db-load-survey-result';
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result.interface';
+import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-mongo.repository';
+import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo.repository';
+
+export const makeDbLoadSurveyResult = (): LoadSurveyResult => {
+  const surveyResultMongoRepository = new SurveyResultMongoRepository();
+  const surveyMongoRepository = new SurveyMongoRepository();
+
+  return new DbLoadSurveyResult(
+    surveyResultMongoRepository,
+    surveyMongoRepository
+  );
+};
--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 import { adaptRoute } from '@/main/adapters/express-route-adapter';
 import { makeSaveSurveyResultController } from '@/main/factories/controller/survey-result/save-survey-result/save-survey-result-controller.factory';
 import { auth } from '@/main/middlewares/auth';
+import { makeLoadSurveyResultController } from '../factories/controller/survey-result/load-survey-result/load-survey-result-controller.factory';
 
 export default (router: Router): void => {
   router.put(
     '/surveys/:surveyId/results',
     auth,
     adaptRoute(makeSaveSurveyResultController())
+  );
+
+  router.get(
+    '/surveys/:surveyId/results',
+    auth,
+    adaptRoute(makeLoadSurveyResultController())
   );
 };
